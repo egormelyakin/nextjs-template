33b475701d859fe1d347c6402cff257f37b23470
Egor Melyakin <foodvegan5@gmail.com>
2025-08-22 15:47:49 +0300

    eslint ts compatible config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,61 +1,70 @@
 import { FlatCompat } from "@eslint/eslintrc";
-import js from "@eslint/js";
+import eslint from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
 import boundaries from "eslint-plugin-boundaries";
 import checkFile from "eslint-plugin-check-file";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
+import tseslint from "typescript-eslint";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
+  recommendedConfig: eslint.configs.recommended,
+  allConfig: eslint.configs.all,
 });
 
 const projectFolders = ["assets", "components", "config", "hooks", "lib", "stores", "testing", "types", "utils"];
 
-const settings = {
-  "boundaries/include": ["src/**/*"],
-  "boundaries/elements": [
-    { mode: "full", type: "shared", pattern: projectFolders.map((folder) => `src/${folder}/**/*`) },
-    { mode: "full", type: "feature", capture: ["featureName"], pattern: ["src/features/*/**/*"] },
-    { mode: "full", type: "app", capture: ["_", "fileName"], pattern: ["src/app/**/*"] },
-  ],
-};
-
-const boundariesRules = {
-  default: "disallow",
-  rules: [
-    { from: "shared", allow: ["shared"] },
-    { from: "feature", allow: ["shared", ["feature", { featureName: "${from.featureName}" }]] },
-    { from: "app", allow: ["app", "shared", "feature"] },
-  ],
-};
-
-const rules = {
-  "prefer-arrow-callback": "error",
-  "prefer-template": "error",
-  "prefer-const": "error",
-  semi: ["error", "always"],
-  quotes: ["error", "double"],
-
-  "check-file/filename-naming-convention": [
-    "error",
-    { "**/*.{ts,tsx}": "KEBAB_CASE" },
-    { ignoreMiddleExtensions: true },
-  ],
-  "check-file/folder-naming-convention": ["error", { "src/**": "KEBAB_CASE" }],
-
-  "boundaries/no-unknown": "error",
-  "boundaries/no-unknown-files": "error",
-  "boundaries/element-types": ["error", boundariesRules],
-};
-
-const config = [
+export default tseslint.config(
   ...compat.extends("next/core-web-vitals", "next/typescript", "prettier"),
-  { plugins: { "check-file": checkFile, boundaries }, ...{ settings, rules } },
-  js.configs.recommended,
-];
+  eslint.configs.recommended,
+  tseslint.configs.recommendedTypeChecked,
+  tseslint.configs.stylisticTypeChecked,
+  {
+    plugins: { "check-file": checkFile, boundaries },
+    settings: {
+      "boundaries/elements": [
+        { mode: "full", type: "shared", pattern: projectFolders.map((folder) => `src/${folder}/**`) },
+        { mode: "full", type: "feature", capture: ["featureName"], pattern: ["src/features/*/**"] },
+        { mode: "full", type: "app", pattern: ["src/app/**"] },
+      ],
+      "boundaries/include": ["src/**/*"],
+    },
+    rules: {
+      "boundaries/no-unknown": "error",
+      "boundaries/no-unknown-files": "error",
+      "boundaries/element-types": [
+        "error",
+        {
+          default: "disallow",
+          message: "Can't import ${dependency.type} files from ${file.type} files",
+          rules: [
+            { from: "shared", allow: ["shared"] },
+            { from: "feature", allow: ["shared", ["feature", { featureName: "${from.featureName}" }]] },
+            { from: "app", allow: ["app", "shared", "feature"] },
+          ],
+        },
+      ],
 
-export default config;
+      "prefer-arrow-callback": "error",
+      "prefer-template": "error",
+      "prefer-const": "error",
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-misused-promises": ["error", { checksVoidReturn: false }],
+      "@next/next/no-img-element": "off",
+      semi: ["error", "always"],
+      quotes: ["error", "double"],
+
+      "check-file/filename-naming-convention": [
+        "error",
+        { "**/*.{ts,tsx}": "KEBAB_CASE" },
+        { ignoreMiddleExtensions: true },
+      ],
+      "check-file/folder-naming-convention": ["error", { "src/**": "KEBAB_CASE" }],
+    },
+    languageOptions: { parserOptions: { projectService: true, tsconfigRootDir: import.meta.dirname } },
+  },
+  eslintConfigPrettier
+);
